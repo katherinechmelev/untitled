aa0143742bc278f3354237b8d9ca41c40214819e
1E10 Laptop <student@bths.edu>
2022-09-30 12:32:55 -0400

    type first class

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class RectangleRunner {
+public class RectangleRunner {
+    public static void main(String[] args){
+//       Rectangle plot1 = new Rectangle(150,200);
+//       Rectangle plot2 = new Rectangle(125);
+//       Rectangle plot3 = new Rectangle();
+//
+//       plot1.setWidth(75);
+//       plot2.setLength(75);
+//       plot2.setWidth(75);
+//       plot3.setWidth(75);
+//
+//       int area1 = plot1.calculateArea();
+//       int area2 = plot2.calculateArea();
+//       int area3 = plot3.calculateArea();
+//
+//       System.out.println("These three pots requires " + (area1+area2+area3) + " square feet of seed");
+//
+       int len = 65;
+       int wid = len + 10;
+       Rectangle rect1 = new Rectangle(len, wid);
+//       Rectangle rect2 = new Rectangle(10.0, 30.0);
+//       Rectangle rect3 = new Rectangle(0);
+
+
+    }
 }
